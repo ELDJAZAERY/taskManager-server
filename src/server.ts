a00d03d5b4cbf5723ef49d421e99c5c2e54f12ca
eldjazaery <eldjazaeryibrahim@gmail.com>
2020-08-22 22:42:26 +0100

    fix PORT

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,25 @@
-import config from 'config';
-import App from './app';
-import DataBase from './config/db';
-import DBsEnum from './config/db/dbs.enums';
-import { seed } from './SEED/init';
+import config from "config";
+import App from "./app";
+import DataBase from "./config/db";
+import DBsEnum from "./config/db/dbs.enums";
+import { seed } from "./SEED/init";
 
 const startServer = async () => {
-  const serverConfig: any = config.get('server');
+  const serverConfig: any = config.get("server");
 
   const DB = new DataBase(DBsEnum.Postgres);
   const connected = await DB.connectDB();
 
   if (connected) {
-    const app = new App(serverConfig.port);
+    const app = new App(process.env.PORT || serverConfig.port);
     app.listen();
 
     // seed Data
-    seed()
-
+    seed();
   } else {
     console.error(
       new Error(
-        '#### cannot start the Server Without ensure the DB connection ####'
+        "#### cannot start the Server Without ensure the DB connection ####"
       )
     );
   }
